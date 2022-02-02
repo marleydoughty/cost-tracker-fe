fbe36db593c6ec4bbe54036d65a9d3c998dc7574
Marley Doughty <marleydoughty@yahoo.com>
2022-02-02 07:14:42 +0000

    started new repo, installed react router dom and material ui for front end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Button} from '@mui/material'
+import styled from '@emotion/styled'
 
 function App() {
+  const [counter, setCounter] = React.useState(0)
+  const handleClick = () => {
+    setCounter(0)
+  }
+  React.useEffect( () => {
+    setInterval( () => {
+      setCounter( (c) => c + 1)
+    }, 1000)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <ContainerOuter>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            You've clicked <code>{counter}</code> times.
         </p>
+          <MySpecialButton onClick={handleClick} variant="contained">Hello there!</MySpecialButton>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,9 +29,30 @@ function App() {
         >
           Learn React
         </a>
+        </ContainerOuter>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const ContainerOuter = styled.div`
+  display: flex;
+  background-color: white;
+  color: black;
+  flex-direction: column;
+  align-items: stretch;
+  padding: 20px;
+  box-shadow: 0 2px 4px rgba(0,0,0,.4);
+  border-radius: 5px;
+
+  > *:not(:last-child) {
+    margin-bottom: 10px;
+  }
+`
+
+const MySpecialButton = styled(Button)`
+  background-color: green;
+  margin: 20px;
+`
